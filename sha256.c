f4367d65057483b82037a1fe05386f96b92bbaa2
Usman Sattar <G00345816@gmit.ie>
2020-03-11 16:29:46 +0000

    Maj Function (SHA-256)

--- sha256.c
+++ sha256.c
@@ -1,4 +1,5 @@
 // Usman Sattar
+
 // https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
 // The secure hash algorithm 256-bit verion.
 #include <stdio.h>
@@ -6,7 +7,12 @@
 
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
   // Section 4.1.2
-  return (x & y) ^ (!x & z);
+  return (x & y) ^ (~x & z);
+}
+
+uint32_t Maj (uint32_t x, uint32_t y, uint32_t z){
+  // Section 4.1.2
+  return (x & y) ^ (x & z) ^ (y & z);
 }
 
 
@@ -17,11 +23,11 @@ int main(int argc, char *argv[]){
   uint32_t y = 0xcccccccc;
   uint32_t z = 0x55555555;
  
-  printf("x          = %08x\n", x);
-  printf("y          = %08x\n", y);
-  printf("z          = %08x\n", z);
+  printf("x           = %08x\n", x);
+  printf("y           = %08x\n", y);
+  printf("z           = %08x\n", z);
 
-  printf("Ch (x,y,z) = %08x\n", Ch(x, y, z));
-
+  printf("Ch (x,y,z)  = %08x\n", Ch(x, y, z));
+  printf("Maj (x,y,z) = %08x\n", Maj(x, y, z));
   return 0;
 }
