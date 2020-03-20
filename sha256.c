993b7cc71a33773651610e7cef950a1272a6413c
Usman Sattar <G00345816@gmit.ie>
2020-03-20 19:15:35 +0000

    Testing and Refactoring SHA256

--- sha256.c
+++ sha256.c
@@ -4,9 +4,13 @@
 // The secure hash algorithm 256-bit verion.
 #include <stdio.h>
 #include <inttypes.h>
+#include <endian.h>
+
+// Definitions of a word - Section 2.1
+#define WORD uint32_t
 
 // Section 4.2.2
-const uint32_t K[] = {
+const WORD K[] = {
   0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
   0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
@@ -25,97 +29,35 @@ const uint32_t K[] = {
   0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 };
 
-uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
-  // Section 4.1.2
-  return (x & y) ^ (~x & z);
-}
+// Section 4.1.2
+#define Ch(x, y, z) ((x & y) ^ (~x & z))
+#define Maj(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
+#define SHR(x, n) (x >> n)
+#define ROTR(x, n) ((x >> n) | (x << (32 - n)))
+#define Sig0(x) (ROTR(x,  2) ^ ROTR(x, 13) ^ ROTR(x, 22))
+#define Sig1(x) (ROTR(x,  6) ^ ROTR(x, 11) ^ ROTR(x, 25))
+#define sig0(x) (ROTR(x,  7) ^ ROTR(x, 18) ^ SHR(x, 3))
+#define sig1(x) (ROTR(x, 17) ^ ROTR(x, 19) ^ SHR(x, 10))
 
-uint32_t Maj (uint32_t x, uint32_t y, uint32_t z){
-  // Section 4.1.2
-  return (x & y) ^ (x & z) ^ (y & z);
-}
-
-uint32_t SHR(uint32_t x, int n){
-  // Section 3.2
-  return x >> n;
-}
-
-uint32_t ROTR(uint32_t x, int n){
-  // Section 3.2
-  return (x >> n) | (x << (32 - n));
-}
-
-uint32_t Sig0(uint32_t x){
-  //Section 4.1.2
-  return ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22);
-}
-
-uint32_t Sig1(uint32_t x){
-  //Section 4.1.2
-  return ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25);
-}
-
-uint32_t sig0(uint32_t x){
-  //Section 4.1.2
-  return ROTR(x, 7) ^ ROTR(x, 18) ^ SHR (x, 3);
-}
-
-uint32_t sig1(uint32_t x){
-  //Section 4.1.2
-  return ROTR(x, 17) ^ ROTR(x, 19) ^ SHR (x, 10);
-}
-
-union block {
+// A sixty-four byte block of memory, accessed with different types.
+typedef union {
   uint64_t sixfour[8];
   uint32_t threetwo[16];
   uint8_t eight[64];
-};
+} BLOCK;
 
-enum flag {READ, PAD0, FINISH};
+// Keep track of where we are in padding the message.
+typedef enum {READ, PAD0, FINISH} PADFLAG;
 
-int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum flag *status) {
+// Section 6.2.2
+void nexthash(WORD *M, WORD *H) {
 
-  if (*status == FINISH)
-    return 0;
-
-  if (*status == PAD0) {
-    for (int i = 0; i < 56; i++)
-      M->eight[i] = 0;
-    M->sixfour[7] = *nobits;
-    *status = FINISH;
-    return 1;
-  }
-
-  size_t nobytesread = fread(M->eight, 1, 64, infile);
-  if (nobytesread == 64)
-    return 1;
-
-  // If we can fit all padding in last block:
-  if (nobytesread < 56) {
-    M->eight[nobytesread] = 0x80;
-    for (int i = nobytesread + 1; i < 56; i++)
-      M->eight[i] = 0;
-    M->sixfour[7] = *nobits;
-    *status = FINISH;
-    return 1;
-  }
-  
-  // Otherwise we have read between 56 (incl) and 64 (excl) bytes.
-  M->eight[nobytesread] = 0x80;
-  for (int i = nobytesread + 1; i < 64; i++)
-    M->eight[i] = 0;
-  *status = PAD0;
-  return 1;
-}
-
-void nexthash(union block *M, uint32_t *H){
-  // Section 6.2.2
-  uint32_t W[64];
-  uint32_t a, b, c, d, e, f, g, h, T1, T2;
+  WORD W[64];
+  WORD a, b, c, d, e, f, g, h, T1, T2;
   int t;
 
   for (t = 0; t < 16; t++)
-    W[t] = M->threetwo[t];
+    W[t] = M[t];
 
   for (t = 16; t < 64; t++)
     W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
@@ -124,49 +66,100 @@ void nexthash(union block *M, uint32_t *H){
   e = H[4]; f = H[5]; g = H[6]; h = H[7];
 
   for (t = 0; t < 64; t++) {
-    T1 = h + Sig1(e) + Ch (e, f, g) + K[t] + W[t];
+    T1 = h + Sig1(e) + Ch(e, f, g) + K[t] + W[t];
     T2 = Sig0(a) + Maj(a, b, c);
     h = g; g = f; f = e; e = d + T1;
     d = c; c = b; b = a; a = T1 + T2;
-    
-    H[0] = a + H[0]; H[1] = b + H[1]; H[2] = c + H[2]; H[3] = d + H[3];
-    H[4] = e + H[4]; H[5] = f + H[5]; H[6] = g + H[6]; H[7] = f + H[7];
-   }
+  }
+
+  H[0] += a; H[1] += b ; H[2] += c; H[3] += d;
+  H[4] += e; H[5] += f ; H[6] += g; H[7] += h;
+
 }
 
-int main(int argc, char *argv[]){
+// Section 5.1.1 - message input from infile.
+int nextblock(BLOCK *M, FILE *infile, uint64_t *nobits, PADFLAG *status) {
 
-  if (argc !=2){
+  int i;
+  size_t nobytesread;
+
+  switch(*status){
+    case FINISH:
+      return 0;
+    case PAD0:
+      // We need an all-padding block without the 1 bit.
+      for (int i = 0; i < 56; i++)
+        M->eight[i] = 0x00;
+      M->sixfour[7] = htobe64(*nobits);
+      *status = FINISH;
+      break;
+    default:
+      // Try to read 64 bytes from the file.
+      nobytesread = fread(M->eight, 1, 64, infile);
+      *nobits += (8ULL * ((uint64_t) nobytesread));
+
+      if (nobytesread < 56) {
+        // We can put all padding in this block.
+        M->eight[nobytesread] = 0x80;
+        for (i = nobytesread + 1; i < 56; i++)
+          M->eight[i] = 0x00;
+        M->sixfour[7] = htobe64(*nobits);
+        *status = FINISH;
+      } else if (nobytesread < 64) {
+        // Otherwise we have read between 56 (incl) and 64 (excl) bytes.
+        M->eight[nobytesread] = 0x80;
+        for (int i = nobytesread + 1; i < 64; i++)
+          M->eight[i] = 0x00;
+        *status = PAD0;
+      }
+  }
+
+  // Convert to host endianess, word-size-wise.
+  for (i = 0; i < 16; i++)
+    M->threetwo[i] = be32toh(M->threetwo[i]);
+
+  return 1;
+
+}
+
+int main(int argc, char *argv[]) {
+
+  // One Arguement Exactly
+  // Expect and open a sinle filename
+  if (argc != 2) {
     printf("Error: expected single filename as argument.\n");
     return 1;
   }
 
+  // Read The File
   FILE *infile = fopen(argv[1], "rb");
-  if (!infile){
+  if (!infile) {
     printf("Error: couldn't open file %s.\n", argv[1]);
     return 1;
   }
-  
+
   // Section 5.3.3
-  uint32_t H[] = {
+  WORD H[] = {
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
     0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
   };
-  
-  // The current padded message block
-  union block M;
+
+  // The current padded message block.
+  BLOCK M;
   uint64_t nobits = 0;
-  enum flag status = READ;
-  // Read through all of the padded message blocks
+  PADFLAG status = READ;
+
+  // Read through all of the padded message blocks.
   while (nextblock(&M, infile, &nobits, &status)) {
-    // Calculate the next hash value
-    // & means address of
-    nexthash(&M,  H);
+    // Calculate the next hash value.
+    nexthash(M.threetwo, H);
   }
-  
+
+  // Print the hash.
   for (int i = 0; i < 8; i++)
-    printf("%02" PRIx32, H[i]);
+    printf("%08" PRIx32 "", H[i]);
   printf("\n");
+
   fclose(infile);
 
   return 0;
